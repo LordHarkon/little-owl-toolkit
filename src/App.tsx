d367c6bc5dc1102622b1fbd633f5d12e6ac40205
LordHarkon <frez758@gmail.com>
2023-04-05 17:01:34 +0300

    initial project files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
+
+function App() {
+  return (
+    <div>
+      <div>Test Text</div>
+      <div>Test Text</div>
+      <div>Test Text</div>
+    </div>
+  );
+}
+
+export default App;
