f6bfbe52fd0fe9a9bdfbe8a328c7bc5baf1a82c0
LordHarkon <frez758@gmail.com>
2023-04-10 17:45:27 +0300

    changed extension for tailwind file

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      spacing: {
+        "13": "3.25rem",
+        "15": "3.75rem",
+        "17": "4.25rem",
+        "18": "4.5rem",
+        "19": "4.75rem",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
