83549d25ab32cccc9fc841f0db2249133aeb81ec
LordHarkon <frez758@gmail.com>
2023-04-10 17:45:44 +0300

    Set up router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
+import { appWindow } from "@tauri-apps/api/window";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import Main from "./components/layouts/Main";
+import ThemeProvider from "./context/ThemeContext";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import Settings from "./pages/Settings";
+import Test from "./pages/Test";
 import "./styles.css";
+
+// TODO: Make these load from a config file
+// Window Settings
+appWindow.setAlwaysOnTop(true);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ThemeProvider>
+      <MemoryRouter>
+        <Main>
+          <Routes>
+            <Route index path="/" element={<Home />} errorElement={<Error />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/test" element={<Test />} />
+          </Routes>
+        </Main>
+      </MemoryRouter>
+    </ThemeProvider>
+  </React.StrictMode>,
 );
